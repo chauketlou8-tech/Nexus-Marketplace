eefe9149db0926023f8e3738cd008c3d45ceb2eb
Chauke Tlou <chauketlou8@gmail.com>
2026-04-09 21:50:06 +0200

    added few files to web jut to get started.

---
+++ frontend/web/src/pages/LoginPage.tsx
@@ -0,0 +1,5 @@
+export default function LoginPage() {
+    return (
+        <h2>Welcome to the nexus-Marketplace login page.</h2>
+    )
+}
---
+++ frontend/web/src/pages/SignupPage.tsx
@@ -0,0 +1,5 @@
+export default function SignupPage() {
+    return (
+        <h2>Welcome to the nexus-Marketplace signup page.</h2>
+    )
+}
